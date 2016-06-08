dd1d80df14bdfc8b9baee454af1365ae8d86f3e9
qindongliang <18210507492@126.com>
2016-06-08 15:13:32 +0800

    check:词库优化控制

--- src/main/java/com/anytrust/tools/DictTools.java
+++ src/main/java/com/anytrust/tools/DictTools.java
@@ -1,5 +1,6 @@
 package com.anytrust.tools;
 
+import com.anytrust.model.Word;
 import com.google.common.collect.Lists;
 import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
@@ -11,31 +12,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by qindongliang on 2015/12/2.
- * 初始化词典数据使用
+ * Created by qindongliang on 2015/12/2. 初始化词典数据使用
  */
 public class DictTools {
 
-    /**主词典*/
-    public static List<String> main_kws=new ArrayList<String>();
-    /**辅助词典**/
-    public static List<String> assist_kws=new ArrayList<String>();
-    /**log记录**/
-    static Logger log=LoggerFactory.getLogger(DictTools.class);
+	/** log记录 **/
+	private static final Logger LOG = LoggerFactory.getLogger(DictTools.class);
+	
+	/** 主词典 */
+	public static List<Word> main_kws = new ArrayList<Word>();
+	/** 辅助词典 **/
+	public static List<String> assist_kws = new ArrayList<String>();
+	
+
+	static {
+		try {
+			// 读取resource下文件使用下面代码，能够在IDE里面使用，也能在打成jar包后，单独使用
+			InputStream mainStream = DictTools.class.getClassLoader().getResourceAsStream("main.txt");
+			String main = CharStreams.toString(new InputStreamReader(mainStream, "UTF-8"));
+			InputStream assitStream = DictTools.class.getClassLoader().getResourceAsStream("assist.txt");
+			String assist = CharStreams.toString(new InputStreamReader(assitStream, "UTF-8"));
+			main_kws =buildModelList(main.split("\n"));
+			assist_kws = Lists.newArrayList(assist.split("\n"));
+		} catch (Exception e) {
+			LOG.error("初始化词典错误!", e);
+		}
+	}
+
+	/***
+	 * 加载词库，组装集合对象
+	 * @param datas
+	 * @return 构建数据集合
+     */
+	static List<Word> buildModelList(String datas[]){
+		List<Word> words=Lists.newArrayList();
+		for(String line:datas){
+
+			if(line.trim().length()<1){
+				continue;
+			}
 
 
-    static {
-        try {
-            //读取resource下文件使用下面代码，能够在IDE里面使用，也能在打成jar包后，单独使用
-            InputStream mainStream = DictTools.class.getClassLoader().getResourceAsStream("main.txt");
-            String  main = CharStreams.toString(new InputStreamReader(mainStream, "UTF-8" ));
-            InputStream assitStream  = DictTools.class.getClassLoader().getResourceAsStream("assist.txt");
-            String assist = CharStreams.toString(new InputStreamReader(assitStream, "UTF-8" ));
-             main_kws= Lists.newArrayList(main.split("\n"));
-             assist_kws=Lists.newArrayList(assist.split("\n"));
-        }catch (Exception e){
-            log.error("初始化词典错误!",e);
-        }
-    }
+			String ds[]=line.trim().split("#");
+
+			Word word=new Word();
+			word.setWord(ds[0]);//设置word
+			if(ds.length==2){
+				word.setDistance(Integer.parseInt(ds[1]));//设置词间距
+			}
+			words.add(word);
+		}
+		return words;
+	}
+
+
+
+
+
+
+
 
 }
